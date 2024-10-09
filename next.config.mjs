e0f2caaad4215af83cf7ec7e7cbd9d4f9dba1fe4
hamasaki-code <dazhibinqi@gmail.com>
2024-10-09 22:48:31 +0900

    add deploy.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/<repo-name>/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/<repo-name>' : '',
+};
